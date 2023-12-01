ab33709a19e8c839d72faeb7819e089cafe85c17
AbdennacerABIDI <aabidi@mobelite.fr>
2023-12-01 10:36:12 +0100

    add flow layout

--- app/src/main/java/com/example/composearchitecture/feature/home/HomeScreen.kt
+++ app/src/main/java/com/example/composearchitecture/feature/home/HomeScreen.kt
@@ -1,23 +1,46 @@
 package com.example.composearchitecture.feature.home
 
 import android.util.Log
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material.icons.outlined.Close
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -27,9 +50,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.util.fastForEachIndexed
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.lifecycle.Lifecycle
@@ -40,6 +69,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.composearchitecture.data.DataProvider
 import com.example.composearchitecture.data.Puppy
 import com.example.composearchitecture.ui.theme.ComposedLibThemeSurface
+import com.example.composearchitecture.ui.theme.Purple500
 
 @Composable
 fun rememberLifecycleEvent(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.current): Lifecycle.Event {
@@ -55,6 +85,8 @@ fun rememberLifecycleEvent(lifecycleOwner: LifecycleOwner = LocalLifecycleOwner.
     }
     return state
 }
+
+@OptIn(ExperimentalLayoutApi::class, ExperimentalMaterial3Api::class)
 @Composable
 internal fun HomeScreen(onClick: () -> Unit) {
 
@@ -69,10 +101,12 @@ internal fun HomeScreen(onClick: () -> Unit) {
     }
 
 
-
     val titles = listOf("Tab 1", "Tab 2", "Tab 3")
     var tabIndex by remember { mutableIntStateOf(0) }
     val puppies = remember { DataProvider.puppyList }
+    var items by remember { mutableStateOf(listOf<String>()) }
+    var i by remember { mutableStateOf(Int) }
+    val username = remember { mutableStateOf(TextFieldValue()) }
 
     Scaffold(topBar = {
         TabRow(selectedTabIndex = tabIndex) {
@@ -83,41 +117,106 @@ internal fun HomeScreen(onClick: () -> Unit) {
                     onClick = { tabIndex = index })
             }
         }
-    }) { paddingValues ->
-
-        if (tabIndex == 0) {
-            AndroidView(
-                factory = { ctx ->
-                    RecyclerView(ctx).apply {
-                        layoutManager = LinearLayoutManager(ctx)
-                        adapter = HomeAdapter()
+    }, content = { paddingValues ->
+        Surface(modifier = Modifier.padding(2.dp, paddingValues.calculateTopPadding(), 2.dp, 2.dp)) {
+            if (tabIndex == 0) {
+                AndroidView(
+                    factory = { ctx ->
+                        RecyclerView(ctx).apply {
+                            layoutManager = LinearLayoutManager(ctx)
+                            adapter = HomeAdapter()
+                        }
+                    },
+                    update = { recyclerView ->
+                        (recyclerView.adapter as? HomeAdapter)?.submitList(puppies)
                     }
-                },
-                update = { recyclerView ->
-                    (recyclerView.adapter as? HomeAdapter)?.submitList(puppies)
-                }
-            )
-            /*LazyColumn(modifier = Modifier.fillMaxWidth(),  contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
-                items(items = puppies, itemContent = {
-                    PuppyListItem(it)
-                })
-            }*/
+                )
+                /*LazyColumn(modifier = Modifier.fillMaxWidth(),  contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp)) {
+                    items(items = puppies, itemContent = {
+                        PuppyListItem(it)
+                    })
+                }*/
 
-        } else {
-            Column(
-                modifier = Modifier
-                    .padding(paddingValues)
-                    .fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Button(onClick) {
-                     Text(text = "Selected page: ${titles[tabIndex]}")
+            } else if (tabIndex == 1) {
+                Column(
+                    modifier = Modifier
+                        .padding(paddingValues)
+                        .fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Button(onClick) {
+                        Text(text = "Selected page: ${titles[tabIndex]}")
+                    }
                 }
+            } else {
+                ConstraintLayout(modifier = Modifier.fillMaxSize()) {
+                    val (flow, textField1, clickableText2) = createRefs()
+                    FlowRow(
+                        modifier = Modifier.constrainAs(flow) {
+                            top.linkTo(parent.top, margin = 16.dp)
+                            linkTo(parent.start, parent.end, bias = 0.1f)
+                            /*start.linkTo(parent.start, margin = 1.dp)
+                            end.linkTo(parent.end, margin = 1.dp)*/
+                        }) {
+                        repeat(items.size) { index ->
+                            Box(
+                                modifier = Modifier
+                                    .wrapContentWidth()
+                                    .padding(4.dp)
+                                    .background(Color.Gray)
+                                    .clickable {
+                                        // Handle item click
+                                        items = items - items[index]
+                                    }
+                            ) {
+                                Text(
+                                    text = items[index],
+                                    color = Color.White,
+                                    modifier = Modifier
+                                        .padding(1.dp)
+                                        .clip(RoundedCornerShape(4.dp))
+                                        .background(Color(0xFF9681EB))
+                                )
+                            }
+                        }
+                    }
+
+                    TextField(
+                        shape = RoundedCornerShape(20.dp),
+                        modifier = Modifier.constrainAs(textField1) {
+                            top.linkTo(flow.bottom, margin = 16.dp)
+                            start.linkTo(parent.start, margin = 16.dp)
+                            end.linkTo(parent.end, margin = 16.dp)
+                            bottom.linkTo(parent.bottom, margin = 16.dp)
+                        }.border(BorderStroke(width = 2.dp, color = Purple500), shape = RoundedCornerShape(20.dp)),
+                        singleLine = true,
+                        label = { Text(text = "Username") },
+                        value = username.value,
+                        onValueChange = { username.value = it }
+                    )
+
+                    Text(text = "Login",
+                        color = Color.White,
+                        modifier = Modifier
+                            .constrainAs(clickableText2) {
+                                top.linkTo(textField1.top, margin = 16.dp)
+                                start.linkTo(parent.start, margin = 16.dp)
+                                end.linkTo(parent.end, margin = 16.dp)
+                            }
+                            .clickable {
+                                items = items + username.value.text
+                                Log.i("items", "HomeScreen: $items")
+                            },
+                        style = TextStyle(fontSize = 40.sp, fontFamily = FontFamily.Cursive)
+                    )
+                }
+
+
             }
-        }
 
-    }
+        }
+    })
 }
 
 @Composable
--- app/src/main/java/com/example/composearchitecture/feature/login/LoginScreen.kt
+++ app/src/main/java/com/example/composearchitecture/feature/login/LoginScreen.kt
@@ -3,15 +3,19 @@ package com.example.composearchitecture.feature.login
 import android.annotation.SuppressLint
 import android.util.Log
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.ClickableText
 import androidx.compose.foundation.text.KeyboardOptions
@@ -22,6 +26,7 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
